51246dfb12e7870fa40cd1d272b75f99e97a621b
DianaJimenez09 <djimenezneculman@gmail.com>
2022-08-22 13:10:06 +0200

    update

--- hola.js
+++ hola.js
@@ -2,7 +2,7 @@ let mysql = require("mysql2");
 let connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Petachis94.",
+  password: "password",
   database: "new_prueba",
 });
 
